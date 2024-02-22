f184b9038d7e2e6556f428f736a58accccf50e8b
Samir Mascarenhas <smascarenhas313@gmail.com>
2024-02-21 22:36:47 -0300

    adicionado operacao

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-def main ():
-        print (2*2)
-        return None
+def gether_data():
+    n1 = input('Primeiro valor: ')
+    n2 = input('Segundo valor: ')
+    op = input('Operação: ')
+
+    return n1, n2, op
+
+def main():
+    n1, n2, op = gether_data()
+
+    print(eval(n1+n2+op))
+
+    return None
 
 
 if __name__ == "__main__":
-        main()
+    main()
